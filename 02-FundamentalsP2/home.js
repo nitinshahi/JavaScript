3e6042e1d2a1e9632143379a133ea0247da5c637
nitin shahi <shahinitin83@gmail.com>
2022-06-18 14:50:27 +0545

    fundamentals02 c2

--- 02-FundamentalsP2/home.js
+++ 02-FundamentalsP2/home.js
@@ -1,1 +1,176 @@
-console.log("nitin");
+'use strict';
+
+/*
+function fruitProcessor(apples,oranges){
+    console.log(apples , oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const nitin = function (age) {
+    console.log(`His age is ${age}`);
+    return 2022-age;
+}
+
+const bornYear = nitin(22);
+console.log(`He was born on ${bornYear}`);
+
+// const sakar = age => 2022-age;
+// const bornYear2 = sakar(24);
+// console.log(`He was born on ${bornYear2}`);
+const sakar = (age,fname) => {
+    console.log(`${fname}'s age is ${age}`);
+    return 2022-age;
+}
+const bornYear2 = sakar(24,`sakar`);
+console.log(`He was born on ${bornYear2}`);
+
+*/
+
+/*
+//////////////////////////////////////
+// Functions
+function logger() {
+  console.log('My name is Jonas');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
+
+
+///////////////////////////////////////
+// Function Declarations vs. Expressions
+
+// Function declaration
+function calcAge1(birthYeah) {
+  return 2037 - birthYeah;
+}
+const age1 = calcAge1(1991);
+
+// Function expression
+const calcAge2 = function (birthYeah) {
+  return 2037 - birthYeah;
+}
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
+
+
+///////////////////////////////////////
+// Arrow functions
+
+const calcAge3 = birthYeah => 2037 - birthYeah;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYeah, firstName) => {
+  const age = 2037 - birthYeah;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas')); console.log(yearsUntilRetirement(1980, 'Bob'));
+
+
+///////////////////////////////////////
+// Functions Calling Other Functions
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} piece of apple and ${orangePieces} pieces of orange.`;
+  return juice;
+}
+console.log(fruitProcessor(2, 3));
+
+
+///////////////////////////////////////
+// Reviewing Functions
+const calcAge = function (birthYeah) {
+  return 2037 - birthYeah;
+}
+
+const yearsUntilRetirement = function (birthYeah, firstName) {
+  const age = calcAge(birthYeah);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired 🎉`);
+    return -1;
+  }
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
+*/
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+A team ONLY wins if it has at least DOUBLE the average score of the other team. Otherwise, no team wins!
+
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)".
+4. Use the 'checkWinner' function to determine the winner for both DATA 1 and DATA 2.
+5. Ignore draws this time.
+
+TEST DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+TEST DATA 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+
+HINT: To calculate average of 3 values, add them all together and divide by 3
+HINT: To check if number A is at least double number B, check for A >= 2 * B. Apply this to the team's average scores 😉
+
+GOOD LUCK 😀
+*/
+
+//Test 2
+const calcAverage = (score1, score2, score3) => (score1+ score2+ score3)/3;
+
+let avgDolphins = calcAverage(85, 54, 71);
+let avgKoalas = calcAverage(23, 34, 27);
+
+const checkWinner = function (teamD, teamK){
+    if (teamD >= 2* teamK) return console.log(`Dolphins win ${teamD} vs ${teamK}`);
+    if (teamD >= 2* teamK) return console.log(`Dolphins win ${teamD} vs ${teamK}`);
+    else{
+        return console.log(`Noone won`);
+    }
+}
+console.log(avgDolphins, avgKoalas);
+checkWinner(avgDolphins, avgKoalas);
+
+//Test 1
+avgDolphins = calcAverage(44, 23, 71);
+avgKoalas = calcAverage(65,54, 49);
+console.log(avgDolphins, avgKoalas);
+checkWinner(avgDolphins, avgKoalas);
