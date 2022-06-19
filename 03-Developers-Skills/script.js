5db33ac46af871401e1b9cdc86051617123064d4
nitin shahi <shahinitin83@gmail.com>
2022-06-19 14:25:08 +0545

    DevelopersSkills c2 finalcommit

--- 03-Developers-Skills/script.js
+++ 03-Developers-Skills/script.js
@@ -131,8 +131,15 @@ TEST DATA 1: [17, 21, 23]
 TEST DATA 2: [12, 5, -5, 0, 4]
 */
 
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
 const printForecast = function (arr) {
+  let str = '';
   for (let i = 0; i < arr.length; i++)
-    console.log(`...${arr[i]}°C in ${i + 1} days `);
+    str += `...${arr[i]}°C in ${i + 1} days `;
+  console.log(str);
 };
 printForecast([17, 21, 23]);
+printForecast(data1);
+printForecast(data2);
