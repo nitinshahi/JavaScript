132f67965e297a8cc98cfdf7f4fa28b684d59933
nitin shahi <shahinitin83@gmail.com>
2022-06-21 21:47:53 +0545

    Guess c3

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,8 +1,6 @@
 'use strict';
 console.log(document.querySelector('.message').textcontent);
 
-document.querySelector(
-  '.message'
-).textContent = `you can change this using DOM`;
+document.querySelector('.message').textContent = ``;
 document.querySelector('.number').textContent = 55;
 document.querySelector('.label-score').textContent = `score score:`;
