e0ccd585b3440bcaa15f43102a8d9bfdc85d510f
nitin shahi <shahinitin83@gmail.com>
2022-06-24 16:03:45 +0545

    JS BTS c2

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -41,3 +41,93 @@ calcAge(1991);
 // console.log(age);
 // printAge();
 */
+
+/*
+///////////////////////////////////////
+// Hoisting and TDZ in Practice
+
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(undefined);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
+*/
+
+// var a;
+// function calcasss(a) {
+//   this.a = a;
+// }
+
+// calcasss(2);
+// console.log(a);
+
+// const nitin = {
+//   a: 10,
+//   nitinMethod: function (a) {
+//     this.a = 10 - a;
+//     return a;
+//   },
+// };
+// console.log(nitin.a);
+// console.log(nitin.nitinMethod(3));
+// nitin.nitinMethod(3);
+// console.log(nitin.a);
+
+// const glo = 20;
+// const n2 = function () {
+//   const glo = 10;
+//   const n3 = function () {
+//     const glo = 5;
+//     console.log(glo);
+//     return this.glo;
+//   };
+//   console.log(n3());
+// };
+
+// n2();
+
+// const firstName = 2;
+// const sakar = {
+//   firstName: 5,
+//   view: () => console.log(this.firstName),
+// };
+
+// sakar.view();
+// console.log(this.firstName);
