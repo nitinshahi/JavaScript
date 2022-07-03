376646ce54a8df361e5ad252da1fb51c753dec5c
nitin shahi <shahinitin83@gmail.com>
2022-07-03 12:40:24 +0545

    set and map c1

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -475,3 +475,75 @@ for (const i of game.scored) {
   scores[i] = scores[i] ? ++scores[i] : 1;
 }
 console.log(scores);
+
+
+/*
+const rest = new Map();
+
+rest.set('name', `ram momo`);
+rest.set(1, `kathmandu, kasthamandap`);
+rest.set(2, `lagan, pokhara`);
+
+rest
+  .set(`categories`, ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set(`open`, 11)
+  .set(`close`, 23)
+  .set(true, `We are Open`)
+  .set(false, `we are close`);
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 8;
+console.log(rest.get(time > rest.get(`open`) && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+console.log(rest.has(2));
+// console.log(rest);
+console.log(rest.size);
+//rest.clear();
+
+const arr = [1, 2];
+rest.set(arr, `Test`);
+rest.set(document.querySelector('h1'), `heading`);
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
+const question = new Map([
+  ['question', `What is the best programming language in the world?`],
+  [1, `java`],
+  [2, 'C'],
+  [3, `JavaScript`],
+  [`correct`, 3],
+  [true, 'correct'],
+  [false, `Wrong Try again`],
+]);
+console.log(question);
+
+//convert objects to map
+console.log(Object.entries(restaurant.openingHours));
+const hoursMap = new Map(Object.entries(restaurant.openingHours));
+console.log(hoursMap);
+
+//Quiiz App
+console.log(question.get(`question`));
+for (const [key, value] of question) {
+  if (typeof key === `number`) {
+    console.log(` Answer ${key} : ${value}`);
+  }
+}
+
+// const answer = Number(prompt(`enter your answer: `));
+// console.log(answer);
+
+// console.log(question.get(question.get('correct') === answer));
+
+console.log([...question]);
+console.log([...question.keys()]);
+console.log([...question.values()]);
+*/
+
