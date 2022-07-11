27b1885786ef0e5966b7b958ead72f14f616cda9
nitin shahi <shahinitin83@gmail.com>
2022-07-11 15:48:32 +0545

    Testing and tweaks c1

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -828,3 +828,25 @@ button.addEventListener('click', function () {
 
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// const arrN = ['nitin', 'god', 'kunal', 'chad', 'nitin'];
+// const setN = new Set(arrN);
+
+// console.log(setN.entries());
+
+// for (const [key, i] of setN.keys()) {
+//   console.log(key, i);
+// }
+
+const stringIn = prompt('Enter a string: ');
+let count = 0;
+for (const i of stringIn) {
+  count++;
+}
+console.log(count);
+
+let a = 1;
+let b = 2;
+[b, a] = [a, b];
+
+console.log(a, b);
